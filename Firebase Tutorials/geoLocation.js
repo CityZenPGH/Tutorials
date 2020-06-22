742762916ff6d29439d030361ec1a9cefc27eb59
Katelyn Morrison <katelyncmorrison98@gmail.com>
2020-06-22 19:12:22 -0400

    firebase tutorials

--- Firebase Tutorials/geoLocation.js
+++ Firebase Tutorials/geoLocation.js
@@ -1,4 +1,4 @@
-(function() {
+(function(){
     var firebaseConfig = {
         apiKey: "AIzaSyBvGmQSKkJ4g7kmsHggOhCKK5Rz6juzBs0",
         authDomain: "cityzen-280417.firebaseapp.com",
@@ -11,68 +11,58 @@
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-
     // Generate a random Firebase location
-    var firebaseRef = firebase.database().ref().push();
-  
+    var database = firebase.database().ref().push();
+    
     // Create a new GeoFire instance at the random Firebase location
-    var geoFireInstance = new firebase.GeoFire(firebaseRef);
-  
+    var geoFireInstance = new geofire.GeoFire(database);
+
+    var showMap = function initMap(latitude, longitude){
+        var curLoc = {lat: latitude, lng: longitude};
+        var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 17, center: curLoc});
+        var marker = new google.maps.Marker({position: curLoc, map: map});
+    }
+    
     /* Uses the HTML5 geolocation API to get the current user's location */
     var getLocation = function() {
-      if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
+        if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
         log("Asking user to get their location");
-        navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
-      } else {
+        navigator.geolocation.getCurrentPosition(geolocationCallback);
+        } else {
         log("Your browser does not support the HTML5 Geolocation API, so this demo will not work.")
-      }
+        }
     };
-  
+    
     /* Callback method from the geolocation API which receives the current user's location */
     var geolocationCallback = function(location) {
-      var latitude = location.coords.latitude;
-      var longitude = location.coords.longitude;
-      log("Retrieved user's location: [" + latitude + ", " + longitude + "]");
-  
-      var username = "wesley";
-      geoFireInstance.set(username, [latitude, longitude]).then(function() {
+        var latitude = location.coords.latitude;
+        var longitude = location.coords.longitude;
+        log("Retrieved user's location: [" + latitude + ", " + longitude + "]");
+        var username = "Katelyn";
+        geoFireInstance.set(username, [latitude, longitude]).then(function() {
         log("Current user " + username + "'s location has been added to GeoFire");
-  
-        // When the user disconnects from Firebase (e.g. closes the app, exits the browser),
-        // remove their GeoFire entry
-        firebaseRef.child(username).onDisconnect().remove();
-  
-        log("Added handler to remove user " + username + " from GeoFire when you leave this page.");
-      }).catch(function(error) {
-        log("Error adding user " + username + "'s location to GeoFire");
-      });
+        });
+
+        geoFireInstance.get("Katelyn", [latitude, longitude]).then(function() {
+            log("Got location of " + latitude + ", " + longitude);
+        });
+
+        showMap(latitude, longitude);
     }
-  
-    /* Handles any errors from trying to get the user's current location */
-    var errorHandler = function(error) {
-      if (error.code == 1) {
-        log("Error: PERMISSION_DENIED: User denied access to their location");
-      } else if (error.code === 2) {
-        log("Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached");
-      } else if (error.code === 3) {
-        log("Error: TIMEOUT: Calculating the user's location too took long");
-      } else {
-        log("Unexpected error code")
-      }
-    };
-  
+    
     // Get the current user's location
     getLocation();
-  
+
     /*************/
     /*  HELPERS  */
     /*************/
     /* Logs to the page instead of the console */
     function log(message) {
-      var childDiv = document.createElement("div");
-      var textNode = document.createTextNode(message);
-      childDiv.appendChild(textNode);
-      document.getElementById("log").appendChild(childDiv);
-    }
-  })();
+        var childDiv = document.createElement("div");
+        var textNode = document.createTextNode(message);
+        childDiv.appendChild(textNode);
+        document.getElementById("log").appendChild(childDiv);
+    };
+})();
+
