fb84983736b194cef7ff8750f24c02b4bb0a6660
Katelyn Morrison <katelyncmorrison98@gmail.com>
2020-07-02 22:19:44 -0400

    Tutorials and stuff

--- Firebase Tutorials/geoLocation.js
+++ Firebase Tutorials/geoLocation.js
@@ -1,6 +1,6 @@
 (function(){
     var firebaseConfig = {
-        apiKey: "AIzaSyBvGmQSKkJ4g7kmsHggOhCKK5Rz6juzBs0",
+        apiKey: "AIzaSyCzSHHJ_u3vD2R2_CX3013GAl-8rYE58r0",
         authDomain: "cityzen-280417.firebaseapp.com",
         databaseURL: "https://cityzen-280417.firebaseio.com",
         projectId: "cityzen-280417",
@@ -13,40 +13,107 @@
     firebase.initializeApp(firebaseConfig);
     // Generate a random Firebase location
     var database = firebase.database().ref().push();
+
+    var iconBase =
+            'icons/';
+
+        var icons = {
+          bicycling: {
+            icon: iconBase + 'cycling.png'
+          },
+		  tree: {
+			icon: iconBase + 'tree.png'
+		  },
+		  cone: {
+			icon: iconBase + 'cone.png'
+          },
+          light: {
+              icon: iconBase + 'light.png'
+          }
+        };
     
     // Create a new GeoFire instance at the random Firebase location
     var geoFireInstance = new geofire.GeoFire(database);
 
+    var markerIdx = 0;
+
+    function decideIdx(iconIdx){
+        markerIdx = iconIdx;
+    }
+
+    // Adds a marker to the map.
+    function addMarker(location, map) {
+        
+        // Add the marker at the clicked location, and add the next-available label
+        // from the array of alphabetical characters.
+        var marker = new google.maps.Marker({
+          position: location,
+          draggable: true,
+          icon: icons[markerIdx].icon,
+          map: map
+        });
+    }
+
     var showMap = function initMap(latitude, longitude){
         var curLoc = {lat: latitude, lng: longitude};
         var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 17, center: curLoc});
-        var marker = new google.maps.Marker({position: curLoc, map: map});
+        document.getElementById('map'), {
+            zoom: 17.5, 
+            center: curLoc,
+            styles: [{
+                featureType: 'poi',
+                stylers: [{ visibility: 'off' }]  // Turn off POI.
+            },
+            {
+                featureType: 'transit.station',
+                stylers: [{ visibility: 'off' }]  // Turn off bus, train stations etc.
+            }],
+            disableDoubleClickZoom: true,
+            streetViewControl: false,
+        });
+
+        // This event listener calls addMarker() when the map is clicked.
+        google.maps.event.addListener(map, 'click', function(event) {
+            addMarker(event.latLng, map);
+        });
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+
+        document.getElementById("cycling").onclick = decideIdx('bicycling');
+
     }
     
     /* Uses the HTML5 geolocation API to get the current user's location */
     var getLocation = function() {
         if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
-        log("Asking user to get their location");
         navigator.geolocation.getCurrentPosition(geolocationCallback);
         } else {
         log("Your browser does not support the HTML5 Geolocation API, so this demo will not work.")
         }
+    };
+
+    var customMarker = function(location, map){
+        // Add the marker at the clicked location, and add the next-available label
+        // from the array of alphabetical characters.
+        var marker = new google.maps.Marker({
+            position: location,
+            draggable: true,
+            icon: icons[markerIdx].icon,
+            map: map
+        });
+
     };
     
     /* Callback method from the geolocation API which receives the current user's location */
     var geolocationCallback = function(location) {
         var latitude = location.coords.latitude;
         var longitude = location.coords.longitude;
-        log("Retrieved user's location: [" + latitude + ", " + longitude + "]");
-        var username = "Katelyn";
-        geoFireInstance.set(username, [latitude, longitude]).then(function() {
-        log("Current user " + username + "'s location has been added to GeoFire");
-        });
-
-        geoFireInstance.get("Katelyn", [latitude, longitude]).then(function() {
-            log("Got location of " + latitude + ", " + longitude);
-        });
+        // geoFireInstance.set(username, [latitude, longitude]).then(function() {
+            
+        // });
+        // geoFireInstance.get("Katelyn", [latitude, longitude]).then(function() {
+        //     log("Got location of " + latitude + ", " + longitude);
+        // });
 
         showMap(latitude, longitude);
     }
@@ -54,15 +121,5 @@
     // Get the current user's location
     getLocation();
 
-    /*************/
-    /*  HELPERS  */
-    /*************/
-    /* Logs to the page instead of the console */
-    function log(message) {
-        var childDiv = document.createElement("div");
-        var textNode = document.createTextNode(message);
-        childDiv.appendChild(textNode);
-        document.getElementById("log").appendChild(childDiv);
-    };
 })();
 
