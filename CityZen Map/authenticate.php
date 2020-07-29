bc31153c2fa3454292dd3ea6dcbddd43df6eee6f
Katelyn Morrison <katelyncmorrison98@gmail.com>
2020-07-28 22:43:30 -0500

    adding login system with mysql

---
+++ CityZen Map/authenticate.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(E_ALL);
+include("config.php");
+session_start();
+
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	exit('Please fill both the username and password fields!');
+}
+
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	$stmt->execute(array($_POST['username']));
+    if ($stmt->rowCount() > 0) {
+	$res = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($_POST['password'], $res['password'])) {
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            header('Location: index.php');
+        } else {
+            echo 'Incorrect password!';
+        }
+    } else {
+        echo 'Incorrect username!';
+    }
+}
+
+
+?>
---
+++ CityZen Map/config.php
@@ -0,0 +1,4 @@
+<?php
+$con = new PDO('mysql:host=localhost;port=8889;dbname=CityZen', 'root', 'root');
+
+?>
